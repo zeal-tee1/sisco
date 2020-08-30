e5bed7ef5a6a0d6931939dc5db7ce94e95c861ad
zeal-tee1 <tawandabundukutu@gmail.com>
2020-08-30 18:46:23 +0100

    gafa

---
+++ src/main/java/com/tess/demo/ThemtApplication.java
@@ -0,0 +1,13 @@
+package com.tess.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThemtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThemtApplication.class, args);
+	}
+
+}
